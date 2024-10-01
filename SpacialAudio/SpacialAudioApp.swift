d78d2f6719f032dbd344bc555db3c8380fd8b2a4
Kanta Oikawa <k.oikawa@kantacky.com>
2024-10-01 19:40:54 +0900

    Initial Commit

---
+++ SpacialAudio/SpacialAudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpacialAudioApp.swift
+//  SpacialAudio
+//
+//  Created by Kanta Oikawa on 2024/10/01.
+//
+
+import SwiftUI
+
+@main
+struct SpacialAudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
